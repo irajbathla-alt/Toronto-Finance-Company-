68653e01d87b3b8227624586e54b5c50aaaf54db
irajbathla-alt <irajbathla@gmail.com>
2026-08-05 19:02:07 -0700

    Update CRM Apps Script deployment URL

--- crm-config.js
+++ crm-config.js
@@ -1,5 +1,5 @@
 window.TFC_CONFIG={
-  apiUrl:"https://script.google.com/macros/s/AKfycbyRhoWiqi6aWtGBzAvI-MKAQ-fwg5TPg0TW-lfkbgDYmwjFUrI4-U18sMwEnarYtUGY/exec",
+  apiUrl:"https://script.google.com/macros/s/AKfycbxeKEo8MZtfQbCla7cCBbThaMkGk-CltycfR2IE2Uk9WcO52sS9ok4Wej_gIOOsI7MP/exec",
   minimumStatements:6,
   demoMode:false
 };
