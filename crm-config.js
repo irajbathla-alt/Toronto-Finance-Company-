89c64b672f5dcf49c5899e1b7fe7ee64561e0568
irajbathla-alt <irajbathla@gmail.com>
2026-08-05 18:41:41 -0700

    Enable live Google CRM mode

--- crm-config.js
+++ crm-config.js
@@ -1,5 +1,5 @@
 window.TFC_CONFIG={
   apiUrl:"https://script.google.com/macros/s/AKfycbxm0erupTtDuVKHBh2KwWSMKZWOmuzfST6ca5QjkecETRok0o1hys1GqwKB04Utx3pH/exec",
   minimumStatements:6,
-  demoMode:true
+  demoMode:false
 };
